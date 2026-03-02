de46c469545ef70b298fc88429f39d0e5b7df9c3
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-02 19:08:57 +0530

    Add files via upload

---
+++ DAY 1/input from users.cpp
@@ -0,0 +1,14 @@
+//PROGRAM TO TAKE INPUT FROM USER
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"ENTER THE VALUE OF A:- ";
+	cin>>a;
+	cout<<"ENTER THE VALUE OF B:-";
+	cin>>b;
+	c=a*b;
+	cout<<"THE MULTIPLICATION IS "<<c;
+	return 0;
+}
