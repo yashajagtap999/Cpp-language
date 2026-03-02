787d112879b2acc00f38c600c022fb51d8d0d144
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-02 19:10:10 +0530

    Add files via upload

---
+++ DAY 2/conditional if.cpp
@@ -0,0 +1,15 @@
+//CONDITIONAL STATEMENTS
+//IF
+#include<iostream>
+using  namespace std;
+int main()
+{
+	int a;
+	cout<<"ENTER THE VALUE OF A:-";
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<"A IS AN EVEN NUMBER";
+	}
+cout<<"A IS AN ODD NUMBER";
+}
