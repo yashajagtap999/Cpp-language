351f1de5e00b43c5bbe70f1dadebfa5061e5e34c
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-03 18:35:28 +0530

    Add files via upload

---
+++ DAY 2/ELSE IF.cpp
@@ -0,0 +1,22 @@
+//ELSE-IF
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1;
+	cout<<"ENTER N1";
+	cin>>n1;
+	if(n1>0)
+	{
+		cout<<"n1 is positive";
+	}
+	else if(n1=0)
+	{
+		cout<<"n1 is zero";
+	}
+	else
+	{
+		cout<<"n1 is negitive\n";
+	}
+	cout<<"out of loop";
+}
