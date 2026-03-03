604df7937087555d2b1562873797bb8d0464b988
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-03 18:34:25 +0530

    Add files via upload

---
+++ DAY 2/IF ELSE.cpp
@@ -0,0 +1,22 @@
+//IF-ELSE
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cout<<"ENTER THE VALUE OF A";
+	cin>>a;
+	cout<<"ENTER THE VALUE OF B";
+	cin>>b;
+	if(a>b)
+	{
+		cout<<"A IS GREATER";
+	}
+	else
+	{
+		cout<<"B IS GREATER";
+	}
+	return 0;
+}
+
