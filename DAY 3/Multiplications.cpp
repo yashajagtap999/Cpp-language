568f9ea8d50b8eba3bffd95bd24e4161ef01616a
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-04 19:13:55 +0530

    Add files via upload

---
+++ DAY 3/Multiplications.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, result;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    result = a * b * c;
+
+    cout << "Multiplication = " << result;
+
+    return 0;
+}
