97cdbeeef00d4c407131ad038e2f85e69fe1f085
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-04 19:12:56 +0530

    Add files via upload

---
+++ DAY 3/Addition of numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, sum;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    sum = a + b + c;
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
