908bd0315b509377ee7e2cbf9342a801f11743c6
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-04 19:07:16 +0530

    Add files via upload

---
+++ DAY 2/SWITCH.cpp
@@ -0,0 +1,24 @@
+//Switch
+#include<iostream>
+using namespace std;
+int main()
+{
+	int sample;
+	cout<<"enter the value";
+	cin>>sample;
+	switch(sample)
+	{
+		case 1:
+			cout<<"enter 1";
+			break;
+		case 2:
+			cout<<"enter 2";
+			break;
+		case 3:
+			cout<<"enter 3";
+			break;
+		default:
+			cout<<"out of loop";
+	}
+	
+}
