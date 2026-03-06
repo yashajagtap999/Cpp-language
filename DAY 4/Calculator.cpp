0ce6135c5d5382b5d3290bed50eef85ff7cfe8f8
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-06 15:46:01 +0530

    Add files via upload

---
+++ DAY 4/Calculator.cpp
@@ -0,0 +1,29 @@
+//Calculator
+#include<iostream>
+using namespace std;
+int main()
+{
+	char op;
+	int a;
+	int b;
+	int c;
+	cout<<"enter operator";
+	cin>>op;
+	cout<<"Enter value of a";
+	cin>>a;
+	cout<<"Enter value of b";
+	cin>>b;
+	switch(op)
+	{
+		case '+':
+			c=a+b;
+			cout<<"the addition is"<<c<<endl;
+			break;
+		case '-':
+			c=a-b;
+			cout<<"the subtraction is"<<c<<endl;
+		default:
+			cout<<"out of loop";
+	}
+	
+	}
