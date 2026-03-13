1ce63971b26665b94ccbc738eb741924f74d01df
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-13 09:25:29 +0530

    Add files via upload

---
+++ DAY 6/Contructor with arguments.cpp
@@ -0,0 +1,24 @@
+//Constructor with arguments
+#include<iostream>
+using namespace std;
+class sample
+{
+	int a;
+	int b;
+	public:
+		sample(int i,int j)
+		{
+			a=i;
+			b=j;
+		}
+		void get()
+		{
+			cout<<"a"<<a;
+			cout<<"b"<<b;
+		}
+};
+int main()
+{
+	sample s1(10,20);
+	s1.get();
+}
