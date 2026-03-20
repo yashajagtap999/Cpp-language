3917f68799221c8e3650a91ceb12e85e600a947b
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-20 13:44:50 +0530

    Add files via upload

---
+++ DAY 7.1/Polymorphism.cpp
@@ -0,0 +1,18 @@
+//Polymorphism
+
+#include<iostream>
+using namespace std;
+void add(int a,int b)
+{
+	cout<<"Addition is"<<a+b;
+	
+}
+void add(float a,float b)
+{
+	cout<<"Addition is"<<a+b;
+}
+int main()
+{
+	add(10,90);
+	add(10.1f,90.1f);
+}
