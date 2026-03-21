99502190ea163a00ded25f053f02513736451217
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-21 18:32:23 +0530

    Add files via upload

---
+++ DAY 7.2/Encapsulation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Bank {
+private:
+    int acc_no;        // private data
+    string name;
+    double balance;
+
+public:
+    // setter methods
+    void setData(int a, string n, double b) {
+        acc_no = a;
+        name = n;
+        balance = b;
+    }
+
+    // getter methods
+    int getAccNo() {
+        return acc_no;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    double getBalance() {
+        return balance;
+    }
+};
+
+int main() {
+    Bank b1;
+
+    b1.setData(101, "Yash", 50000);
+
+    cout << "Account No: " << b1.getAccNo() << endl;
+    cout << "Name: " << b1.getName() << endl;
+    cout << "Balance: " << b1.getBalance() << endl;
+
+    return 0;
+}
