0607799a9cf68fa72192f2bb6c8cdc28410e58ba
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-25 18:56:37 +0530

    Add files via upload

---
+++ DAY 7.1/Decrement Operator Overloading.cpp
@@ -0,0 +1,28 @@
+//Decreament operator overloading
+
+/*#include<iostream>
+using namespace std;
+class sample
+{
+	public:
+		int a;
+	sample(int i)
+	{
+		a=i;
+	}
+	void display()
+	{
+		cout<<"A:--"<<a;
+	}
+	void operator--()
+	{
+		a--;
+	}
+};
+int main()
+{
+	sample s1(100);
+	s1.display();
+	--s1;
+	s1.display();
+}*/
