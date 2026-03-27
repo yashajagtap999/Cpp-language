cdb354e122f30e655601f070bb486dc42b246439
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-27 18:12:54 +0530

    Add files via upload

---
+++ DAY 8.2/Constructor destructor and exception.cpp
@@ -0,0 +1,27 @@
+//Constructor  ,Destructor and Exception
+#include<iostream>
+using namespace std;
+class Sample
+{
+	public:
+		Sample()
+		{
+			cout<<"Constructor invokrd"<<endl;
+		}
+		~Sample()
+		{
+			cout<<"Destructor invoked"<<endl;
+		}
+};
+int main()
+{
+	try
+	{
+		Sample s1;
+		throw(1);
+	}
+	catch(int s1)
+	{
+		cout<<"Exception handled";
+	}
+}
