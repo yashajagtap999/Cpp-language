f117f05420c8215ec761edde8fee9d54f49adf95
Yash Jagtap <yashajagtap1@gmail.com>
2026-03-29 17:57:24 +0530

    Add files via upload

---
+++ DAY 8.4/Exceptional Handling.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 0;
+
+    try {
+        if (b == 0)
+            throw "Division by zero error";
+        cout << a / b;
+    }
+    catch (const char* msg) {
+        cout << "Exception caught: " << msg;
+    }
+
+    return 0;
+}
