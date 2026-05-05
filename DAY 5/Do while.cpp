49f931f3cca3140cc8f0450df167d2126cca0bb0
Yash Jagtap <yashajagtap1@gmail.com>
2026-05-05 12:39:10 +0530

    Update Do while.cpp

--- DAY 5/Do while.cpp
+++ DAY 5/Do while.cpp
@@ -1,4 +1,4 @@
-//A do...while loop in C++ is a control structure that executes a block of code at least once, and then repeats the execution as long as the condition is true
+//A do...while loop in C++ is a control structure that executes a block of code at least once, and then repeats the execution as long as the condition is true.
 #include<iostream>
 using namespace std;
 int main()
